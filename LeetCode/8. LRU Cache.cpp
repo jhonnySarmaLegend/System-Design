ae20fcf8a20565780dc7e682569cd860c0e46045
jhonnySarmaLegend <jhonny.sarma.legend@gmail.com>
2025-07-18 15:40:39 +0530

    Create 8. LRU Cache.cpp

---
+++ LeetCode/8. LRU Cache.cpp
@@ -0,0 +1,73 @@
+// Link --> https://leetcode.com/problems/lru-cache/description/
+
+class LRUCache {
+
+  public:
+    class node {
+      public:
+        int key;
+      int val;
+      node * next;
+      node * prev;
+      node(int _key, int _val) {
+        key = _key;
+        val = _val;
+      }
+    };
+
+  node * head = new node(-1, -1);
+  node * tail = new node(-1, -1);
+
+  int cap;
+  unordered_map < int, node * > m;
+
+  LRUCache(int capacity) {
+    cap = capacity;
+    head -> next = tail;
+    tail -> prev = head;
+  }
+
+  void addnode(node * newnode) {
+  newnode -> next = head -> next;
+  newnode -> prev = head;  
+        
+  head -> next -> prev = newnode;
+  head -> next = newnode;       
+
+  }
+
+  void deletenode(node * delnode) {
+    delnode -> prev -> next = delnode -> next;
+    delnode -> next -> prev = delnode -> prev;
+  }
+
+  int get(int key_) {
+    if (m.find(key_) != m.end()) {
+      node * resnode = m[key_];
+      int res = resnode -> val;
+      m.erase(key_);
+      deletenode(resnode);
+      addnode(resnode);
+      m[key_] = head -> next;
+      return res;
+    }
+
+    return -1;
+  }
+
+  void put(int key_, int value) {
+    if (m.find(key_) != m.end()) {
+      node * existingnode = m[key_];
+      m.erase(key_);
+      deletenode(existingnode);
+    }
+    if (m.size() == cap) {
+      m.erase(tail -> prev -> key);
+      deletenode(tail -> prev);
+    }
+
+    addnode(new node(key_, value));
+    m[key_] = head -> next;
+  }
+
+};
